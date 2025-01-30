55a3201642b7292b9554733130749672838b24ad
Brittany Ellich <brittanyellich@github.com>
2025-01-29 17:22:20 -0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     return (
         <div>
             <Header />
+            <Button text="Pet Portrait Commission Form" onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSefV3dCeSP-UiGSFJh0xFYVZTm00argXWDt-g425K-4r2tOFw/viewform?usp=sharing', '_blank')} />
+       
             <Button text="Commission Form" onClick={() => window.open(config.commissionUrl, '_blank')} />
             <h2>Recent Work</h2>
             <InstagramPosts />
